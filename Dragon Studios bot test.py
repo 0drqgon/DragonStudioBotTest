fbe79c7c731603f0a2d1daca3659757cb2f8e63b
0drqgon <dragonslayer205050@gmail.com>
2023-03-14 17:59:16 +0200

    Update Dragon Studios bot test.py

--- Dragon Studios bot test.py
+++ Dragon Studios bot test.py
@@ -4,25 +4,19 @@ import discord
 from discord.utils import get
 from discord.ext import commands
 
-
-CONFIG_PATH = 'settings.json'
-
-if not os.path.isfile(CONFIG_PATH):
-    with open(CONFIG_PATH, 'w') as config_file:
-        config_file.write('{"token": ""}')
-
-with open('settings.json', 'r') as token_file:
-    data = json.load(token_file)
-TOKEN = data.get('token', None)
-
-GUILD = ''
+TOKEN = 
+GUILD = '1085193543905181737'
 EMOJI_BELL = '🔔'
 EMOJI_GIVEAWAY = '🎉'
-ROLE_BELL_ID = 
-ROLE_GIVEAWAY_ID = 
+ROLE_BELL_ID = 1085193543905181740
+ROLE_GIVEAWAY_ID = 1085193543905181739
+TICKET_CATEGORY_NAME = "Support"  # Name of the ticket category
+TICKET_EMOJIS = ["🎫", "🆘"]
+TICKET_CHANNELS = []
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = commands.Bot(intents=intents, command_prefix="!")
 
@@ -31,6 +25,68 @@ client = commands.Bot(intents=intents, command_prefix="!")
 async def on_ready():
     guild = get(client.guilds, name=GUILD)
     print(f'{client.user} has connected to Discord!')
+
+
+# Ticket system functionality
+@client.event
+async def on_raw_reaction_add(payload):
+    if str(payload.emoji) in TICKET_EMOJIS:
+        channel = client.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        user = await client.fetch_user(payload.user_id)
+        if str(payload.emoji) == ":ticket:":
+            category = discord.utils.get(channel.guild.categories, name=TICKET_CATEGORY_NAME)
+            if not category:
+                category = await channel.guild.create_category(TICKET_CATEGORY_NAME)
+            ticket_name = f"{user.name}-ticket"
+            ticket_channel = await category.create_text_channel(name=ticket_name)
+            TICKET_CHANNELS.append(ticket_channel)
+            await ticket_channel.set_permissions(user, read_messages=True, send_messages=True)
+            await message.remove_reaction(payload.emoji, user)
+            ticket_embed = discord.Embed(
+                title=f"Ticket for {user.name}", description="Please state your issue.", color=0x00ff00
+            )
+            await ticket_channel.send(embed=ticket_embed)
+        elif str(payload.emoji) == ":sos:":
+            category = discord.utils.get(channel.guild.categories, name=TICKET_CATEGORY_NAME)
+            if category:
+                for channel in category.channels:
+                    if channel.name.startswith(user.name):
+                        await channel.delete()
+                        TICKET_CHANNELS.remove(channel)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    pass
+
+# Command to start a new ticket channel with a button
+@client.command()
+async def newticket(ctx):
+    if ctx.message.content == "!newticket":
+        category = discord.utils.get(ctx.guild.categories, name=TICKET_CATEGORY_NAME)
+        if not category:
+            category = await ctx.guild.create_category(TICKET_CATEGORY_NAME)
+
+        ticket_name = f"{ctx.author.name}-ticket"
+        ticket_channel = await category.create_text_channel(name=ticket_name)
+        TICKET_CHANNELS.append(ticket_channel)
+        await ticket_channel.set_permissions(ctx.author, read_messages=True, send_messages=True)
+
+        ticket_embed = discord.Embed(title=f"Ticket for {ctx.author.name}", description="Please state your issue.", color=0x00ff00)
+        message = await ctx.send(
+        "Click the button below to start a new ticket channel.",
+        components=[
+            [discord.ui.Button(style=discord.ButtonStyle.green, label="Open ticket", custom_id="new_ticket")]
+        ]
+    )
+
+    while True:
+        interaction = await client.wait_for("button_click")
+        if interaction.custom_id == "new_ticket":
+            await message.delete()
+            await interaction.respond(type=6)
+            await ticket_channel.send(embed=ticket_embed)
+            break
 
 
 @client.event
